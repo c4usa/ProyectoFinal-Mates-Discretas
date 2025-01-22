cc02ee7b2968506975cea8df84772c9d783f36aa
Tomas <tomas842011@gmail.com>
2025-01-21 19:32:14 -0500

    Corrigiendo Colores, Añadir tabla primaria, mejoras en el código

--- librerias/funciones.h
+++ librerias/funciones.h
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include <string.h>
 
@@ -7,15 +6,15 @@
 #define RED "\033[31m"
 #define BLUE "\033[34m"
 
-void mostrarCarátula()
+void mostrarCaratula()
 {
-    printf(BLUE"*********************************************************************************************************\n");
-    printf(BLUE"\t\t\t\tMATEMÁTICAS DISCRETAS 1\t\t\t\t\n");
+    printf(BLUE "*********************************************************************************************************\n");
+    printf(BLUE "\t\t\t\tMATEMÁTICAS DISCRETAS 1\t\t\t\t\n");
     printf("\n");
-    printf(BLUE"\tIntegrantes: Diego Molina, Tomás González\n");
-    printf(BLUE"\tEste programa genera una expresión SOP a partir de una tabla de verdad ingresada por el usuario\n");
-    printf(BLUE"\tAño Electivo: 2024-2025\n");
-    printf(BLUE"********************************************************************************************************\n\n");
+    printf(BLUE "\tIntegrantes: Diego Molina, Tomás González\n");
+    printf(BLUE "\tEste programa genera una expresión SOP a partir de una tabla de verdad ingresada por el usuario\n");
+    printf(BLUE "\tAño Electivo: 2024-2025\n");
+    printf(BLUE "********************************************************************************************************\n\n");
 }
 
 int ingresarNumeroVariables()
@@ -23,7 +22,7 @@ int ingresarNumeroVariables()
     int variables;
     do
     {
-        printf(BLUE"Digite el número de variables lógicas (2 o 3): ");
+        printf(BLUE "Digite el número de variables lógicas (2 o 3): ");
         scanf("%d", &variables);
         if (variables != 2 && variables != 3)
         {
@@ -33,9 +32,9 @@ int ingresarNumeroVariables()
     return variables;
 }
 
-void ingresarValores(int variables, int filas, int salida[])
+void ingresarValores(int filas, int salida[])
 {
-    printf(BLUE"\nIngrese los valores de salida, 0 o 1 para cada fila en la tabla de verdad:\n");
+    printf(BLUE "\nIngrese los valores de salida, 0 o 1 para cada fila en la tabla de verdad:\n");
     for (int i = 0; i < filas; i++)
     {
         printf("Fila %d: ", i + 1);
@@ -50,7 +49,30 @@ void ingresarValores(int variables, int filas, int salida[])
     }
 }
 
-void generarExpresion(int variables, int filas, int salida[], char expresion[])
+void generarTablaPrimaria(int variables, int filas, int salida[])
+{
+    printf(BLUE "\nTabla primaria:\n" RESET);
+    printf("\t");
+    for (int i = 0; i < variables; i++)
+    {
+        printf(BLUE "%-8c" RESET, 'A' + i);
+    }
+    printf(GREEN "%-10s\n" RESET, "Salida");
+    printf(BLUE "\t-----------------------------------------------------\n" RESET);
+
+    for (int i = 0; i < filas; i++)
+    {
+        printf("\t");
+        for (int j = 0; j < variables; j++)
+        {
+            int valor = (i >> (variables - j - 1)) & 1;
+            printf(BLUE "%-8d" RESET, valor);
+        }
+        printf(GREEN "%-10d\n" RESET, salida[i]);
+    }
+}
+
+void generarExpresion(int variables, int filas, int salida[], char expresion[], char terminos[8][50])
 {
     char term[50];
     expresion[0] = '\0';
@@ -65,42 +87,66 @@ void generarExpresion(int variables, int filas, int salida[], char expresion[])
                 char literal[10];
                 if ((i >> (variables - j - 1)) & 1)
                 {
-                sprintf(literal, "%c", 'A' + j);
+                    sprintf(literal, "%c", 'A' + j);
                 }
                 else
                 {
-                sprintf(literal, "%c'", 'A' + j);
+                    sprintf(literal, "%c'", 'A' + j);
                 }
                 strcat(term, literal);
                 if (j < variables - 1)
-                strcat(term, "*");
+                    strcat(term, "*");
             }
+            strcpy(terminos[i], term);
             if (expresion[0] != '\0')
                 strcat(expresion, " + ");
             strcat(expresion, term);
         }
+        else
+        {
+            terminos[i][0] = '\0';
+        }
     }
 }
 
-int calcularOperacion(int a, int b, char operador)
+int evaluarTermino(int valores[], int variables, const char *termino)
 {
-    if (operador == '|')
-        return a | b;
-    return 0;
+    int resultado = 1;
+    for (int i = 0; termino[i] != '\0'; i++)
+    {
+        if (termino[i] >= 'A' && termino[i] <= 'C')
+        {
+            int indice = termino[i] - 'A';
+            int valor = valores[indice];
+            if (termino[i + 1] == '\'')
+            {
+                valor = !valor;
+                i++;
+            }
+            resultado &= valor;
+        }
+    }
+    return resultado;
 }
 
-void generarTablaDeVerdad(int variables, int filas, int salida[], const char expresion[])
+void generarTablaDeVerdad(int variables, int filas, int salida[], const char expresion[], char terminos[8][50])
 {
-    printf(BLUE"Tabla de verdad de la expresión %s\n", expresion);
+    printf(BLUE "\nTabla de verdad de la expresión %s\n" RESET, expresion);
     printf("\t");
     for (int i = 0; i < variables; i++)
     {
-        printf("%-8c", 'A' + i);
+        printf(BLUE "%-8c" RESET, 'A' + i);
     }
-    if (variables == 3)
-        printf("%-10s",BLUE"Operación");
-    printf("%-10s\n", RED"\tResultado");
-    printf(BLUE"\t-----------------------------------------------------\n");
+    for (int i = 0; i < filas; i++)
+    {
+        if (terminos[i][0] != '\0')
+        {
+            printf(GREEN "%-15s" RESET, terminos[i]);
+        }
+    }
+    printf(RED "%-10s\n" RESET, "Resultado");
+    printf(BLUE "\t--------------------------------------------------------------------------------------------------------------------\n" RESET);
+
     for (int i = 0; i < filas; i++)
     {
         printf("\t");
@@ -108,20 +154,18 @@ void generarTablaDeVerdad(int variables, int filas, int salida[], const char exp
         for (int j = 0; j < variables; j++)
         {
             valores[j] = (i >> (variables - j - 1)) & 1;
-            printf(GREEN "%-8d" RESET, valores[j]);
+            printf(BLUE "%-8d" RESET, valores[j]);
         }
-        int operacion;
-        if (variables == 2)
+        int resultadoFinal = 0;
+        for (int k = 0; k < filas; k++)
         {
-            operacion = calcularOperacion(valores[0], valores[1], '|');
-            printf(BLUE "%-10d" RESET, operacion); 
+            if (terminos[k][0] != '\0')
+            {
+                int resultadoParcial = evaluarTermino(valores, variables, terminos[k]);
+                printf(GREEN "%-15d" RESET, resultadoParcial);
+                resultadoFinal |= resultadoParcial;
+            }
         }
-        else if (variables == 3)
-        {
-            operacion = calcularOperacion(calcularOperacion(valores[0], valores[1], '|'), valores[2], '|');
-            printf(BLUE "%-10d" RESET, operacion); 
-        }
-        printf("\t");
-        printf(RED "%-10d\n" RESET, salida[i]);
+        printf(RED "%-10d\n" RESET, resultadoFinal);
     }
 }
--- main.c
+++ main.c
@@ -1,15 +1,24 @@
 #include <stdio.h>
 #include ".\librerias\funciones.h"
-int main(int argc, char *argv[])
+#include <stdio.h>
+
+int main (int argc, char *argv[]) 
 {
-    mostrarCarátula();
+    mostrarCaratula();
     int variables = ingresarNumeroVariables();
     int filas = (1 << variables);
     int salida[filas];
-    ingresarValores(variables, filas, salida);
+
+    ingresarValores(filas, salida);
+
+    generarTablaPrimaria(variables, filas, salida);
+
     char expresion[100];
-    generarExpresion(variables, filas, salida, expresion);
-    printf("\nLa expresión booleana generada es: %s\n", expresion);
-    generarTablaDeVerdad(variables, filas, salida, expresion);
+    char terminos[8][50];
+    generarExpresion(variables, filas, salida, expresion, terminos);
+
+    printf(BLUE "\nLa expresión booleana generada es: %s\n", expresion);
+
+    generarTablaDeVerdad(variables, filas, salida, expresion, terminos);
     return 0;
 }
